0430fd714649504c1cb763a82dc8d89866de8dd0
kuwako <kuwako@leverages.jp>
2016-02-19 19:12:05 +0900

    完成

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLavel: UILabel!
+    @IBAction func getOmikuji(sender: AnyObject) {
+        let results = [
+            "大吉",
+            "吉",
+            "中吉",
+            "凶",
+            "大凶"
+        ]
+        // 乱数を発生
+        let random = arc4random_uniform(UInt32(results.count))
+        
+        if random == 0 {
+            self.myLavel.textColor = UIColor.redColor()
+        } else {
+            self.myLavel.textColor = UIColor.blackColor()
+        }
+        
+        // results[乱数]をmyLavelに表示
+        self.myLavel.text = results[Int(random)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
